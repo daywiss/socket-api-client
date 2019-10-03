7f0089f16504e0600286d53a05e4753e2c55979f
david <daywiss@gmail.com>
2019-10-03 00:50:50 +0000

    fix some state stuff

--- state.js
+++ state.js
@@ -4,10 +4,10 @@ const unset = require('lodash/set')
 module.exports = (state) => (channel) => {
   return ([path=[],data])=>{
     if(path.length){
-      if(data===undefined){
-        unset(state[channel],path)
+      if(data === undefined || data === null){
+        unset(state,[channel,...path])
       }else{
-        set(state[channel],path,data)
+        set(state,[channel,...path],data)
       }
     }else{
       state[channel] = data
